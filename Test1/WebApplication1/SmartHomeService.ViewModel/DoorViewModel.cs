b7fded6ff92320f00b75017f2bbd340b60c22c32
smarthomeservice <smarthomeservicegithut@gamil.com>
2014-10-07 09:44:32 -0600

    Add One ApiController

---
+++ Test1/WebApplication1/SmartHomeService.ViewModel/DoorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeService.ViewModel
+{
+    public class DoorViewModel
+    {
+        public Guid ID { get; set; }
+        public int DeviceID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Test1/WebApplication1/WebApplication1/Controllers/DoorController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SmartHomeService.ViewModel;
+
+namespace WebApplication1.Controllers
+{
+    public class DoorController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<DoorViewModel> Get()
+        {
+            return new List<DoorViewModel> 
+                {
+                    new DoorViewModel { ID = Guid.NewGuid(), Name = "Hello World", DeviceID = 1 }, 
+                    new DoorViewModel { ID = Guid.NewGuid(), Name = "Hello World2", DeviceID = 2 } 
+                };
+        }
+
+        // GET api/<controller>/5
+        public DoorViewModel Get(int id)
+        {
+            return Get().FirstOrDefault(x=>x.DeviceID == id) ;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
